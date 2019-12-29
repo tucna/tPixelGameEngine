7932b7ab7a6c4511fc489127cc4a5f8c7e1e82b6
Pavel Vlasanek <pavel.vlasanek@tucna.net>
2019-12-29 16:17:01 +0100

    Spacing and doc screenshot

--- tExampleProgram.cpp
+++ tExampleProgram.cpp
@@ -4,37 +4,37 @@
 class Example : public tDX::PixelGameEngine
 {
 public:
-	Example()
-	{
-		sAppName = "Example";
-	}
+  Example()
+  {
+    sAppName = "Example";
+  }
 
 public:
-	bool OnUserCreate() override
-	{
-		// Called once at the start, so create things here
-		return true;
-	}
+  bool OnUserCreate() override
+  {
+    // Called once at the start, so create things here
+    return true;
+  }
 
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		// called once per frame
+  bool OnUserUpdate(float fElapsedTime) override
+  {
+    // called once per frame
     for (int x = 0; x < ScreenWidth(); x++)
       for (int y = 0; y < ScreenHeight(); y++)
         Draw(x, y, tDX::Pixel(rand() % 255, rand() % 255, rand() % 255));
 
-    DrawString(5,5, "TUCNA");
+    DrawString(5, 5, "TUCNA");
 
-		return true;
-	}
+    return true;
+  }
 };
 
 
 int main()
 {
-	Example demo;
-	if (demo.Construct(250, 250, 4, 4))
-		demo.Start();
+  Example demo;
+  if (demo.Construct(250, 250, 4, 4))
+    demo.Start();
 
-	return 0;
+  return 0;
 }
